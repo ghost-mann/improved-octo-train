3a6d4272f8d55b333045f182d87f3a0be9e542a6
ghost-mann <iamaob8@protonmail.com>
2025-02-07 10:02:17 +0300

    feat: added GET http method used in calculation of a number's square in app.py, index.html and answer.html templates

--- app.py
+++ app.py
@@ -1,43 +1,19 @@
-import datetime
-import click
-from flask import Flask,redirect,render_template,request,url_for
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase, mapped_column
+from flask import Flask,render_template, request
 
 app = Flask(__name__)
 
-
-class Base(DeclarativeBase):
-    pass
-# configure sqlite db
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
-
-db = SQLAlchemy(model_class=Base) # instance of sqlalchemy
-
-
-# defining models
-class Event(db.Model):
-    date = mapped_column(db.String, primary_key=True)
-    event = mapped_column(db.String)
-
-db.init_app(app) # linking sqlalchemy instance to flask app
-
-@app.cli.command('initdb')
-def initdb():
-    # create the db
-    with app.app_context():
-        db.create_all()
-        click.echo('Initialized the database.')
-
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def index():
-        if(request.method == 'POST'):
-            db.session.add(Event(date=datetime.datetime.now().__str__(), event=request.form['eventBox']))
-            db.session.commit()
-            return redirect(url_for('index'))
-        return render_template('index.html', eventsList=db.session.execute(db.select(Event).order_by(Event.date)).scalars()) # GET
+    if request.method == 'GET':
+        if (request.args.get('num') == None):
+            return render_template('index.html')
 
+        elif(request.args.get('num') == None):
+            return "<html><body><h1>Invalid Request</h1></body></html>"
+        else:
+            number = request.args.get('num')
+            sq = int(number) * int(number)
+            return render_template('answer.html', squareofnum=sq, num=number)
 
-# starts development server
-if __name__ == '__main__' :
+if(__name__ == '__main__'):
     app.run(debug=True)
