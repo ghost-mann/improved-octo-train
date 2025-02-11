76795977058ed5ea7df5ae76eece18be44891713
ghost-mann <iamaob8@protonmail.com>
2025-02-11 14:00:16 +0300

    feat: implementing a simple flask program in app.py

--- app.py
+++ app.py
@@ -1,19 +1,7 @@
-from flask import Flask,render_template, request
-
+from flask import Flask
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
-def index():
-    if request.method == 'GET':
-        if (request.args.get('num') == None):
-            return render_template('index.html')
-
-        elif(request.args.get('num') == None):
-            return "<html><body><h1>Invalid Request</h1></body></html>"
-        else:
-            number = request.args.get('num')
-            sq = int(number) * int(number)
-            return render_template('answer.html', squareofnum=sq, num=number)
-
-if(__name__ == '__main__'):
-    app.run(debug=True)
+@app.route('/')
+def msg():
+    return 'hello'
+app.run(debug=True)
