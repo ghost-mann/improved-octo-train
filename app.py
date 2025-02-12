cd2f13cd5008cbe9843842c285e044e9f1eafebb
ghost-mann <iamaob8@protonmail.com>
2025-02-12 12:48:21 +0300

    feat: implementing url redirect in app.py and login.html

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template, request
+
 app = Flask(__name__)
 
 @app.route('/')
-def msg():
-    return 'hello'
+def index():
+    return render_template("login.html")
 
-@app.route('/vfloat/<float:weight>')
-def vfloat(weight):
-    return "my weight is %f" % weight
+@app.route('/success')
+def success():
+    return "logged in successfully"
 
-app.run(debug=True)
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST" and request.form["username"] == "admin":
+        return redirect(url_for("success"))
+
+    return redirect(url_for('index'))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
